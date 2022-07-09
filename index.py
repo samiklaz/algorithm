39f0fe0ed1bf9fb02cf8586c976da31f6f39b73c
samiklaz <samuelasuquo80@gmail.com>
2022-07-09 21:34:02 +0100

    Finding the sum of large array elements

--- index.py
+++ index.py
@@ -1,1 +1,28 @@
-print("yes")
+"""
+PROBLEM STATEMENT 
+Calculate and print the sum of the elements in an array.
+Keeping in mind that some integers would be long. 
+
+CONDITIONS
+
+
+SAMPLE INPUT 
+5 
+1000000000 10000003
+
+SAMPLE OUTPUT 
+sum of the input element
+
+CONSTRAINT
+1 <= n <= 10
+0 <= ar[i] <= 10^10
+"""
+
+from array import array
+
+def aVeryBigSum(ar):
+    return sum(ar)
+
+ar = array('i', [1000000000, 10000003, 1000000000, 10000003])
+
+print(aVeryBigSum(ar))
