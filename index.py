1258abf06ae1254b9f078c005b735b00dce406ce
samiklaz <samuelasuquo80@gmail.com>
2022-07-11 11:26:07 +0100

    Write a program that prints a staircase of size n

--- index.py
+++ index.py
@@ -1,14 +1,23 @@
 """
 PROBLEM STATEMENT 
-
+Consider a staircase of size n. Write a program that prints a staircase
+of size n.
 
 CONDITIONS
 
 
 SAMPLE INPUT 
+A single integer, n denoting the sie of the staircase
 
 SAMPLE OUTPUT 
 
-
 CONSTRAINT
+0 <= n <= 100
 """
+
+def staircase(n):
+    for i in range(1, n+1):
+        s = "#" * i 
+        print(s.rjust(n))
+
+staircase(4)
