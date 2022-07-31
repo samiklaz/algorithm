831be7b9358870e5162579216d463ce3f2530005
samiklaz <samuelasuquo80@gmail.com>
2022-07-31 19:42:20 +0100

    Permutation Equation
    Given a sequence of n integers, p(1), p(2),...,p(n) where each element is
    distinct and satisfies 1 <= p(x) <= n. For each x where 1 <= x <= n. Find
    the integer y such that p(p(y)) <=> x and print the value of y on a new
    line.
    
    For example, assume the sequence p = [5, 2, 1, 3, 4]. Each value of x
    between 1 and 5, the length of the sequence is analyzed as follows:
    [4, 2, 5, 1, 3]

--- index.py
+++ index.py
@@ -1,5 +1,15 @@
 """
 PROBLEM STATEMENT 
+Permutation Equation
+
+Given a sequence of n integers, p(1), p(2),...,p(n) where each element is
+distinct and satisfies 1 <= p(x) <= n. For each x where 1 <= x <= n. Find 
+the integer y such that p(p(y)) <=> x and print the value of y on a new 
+line.
+
+For example, assume the sequence p = [5, 2, 1, 3, 4]. Each value of x 
+between 1 and 5, the length of the sequence is analyzed as follows:
+[4, 2, 5, 1, 3]
 
 CONDITIONS
 
@@ -10,3 +20,17 @@ SAMPLE OUTPUT
 CONSTRAINT
 """
 
+def permutationEquation(p):
+    """
+    Parameters:
+        p: an array of integers.
+    """
+    result = []
+    n = len(p)
+    for i in range(1, n+1):
+        result.append(p.index(p.index(i) + 1) + 1)
+    return result
+
+
+p = [5, 2, 1, 3, 4]
+print(permutationEquation(p))
