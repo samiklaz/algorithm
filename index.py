c0be42dd3cf73a24991587b67f51fdc6ffe560ad
samiklaz <samuelasuquo80@gmail.com>
2022-08-10 20:37:06 +0100

    Extra Long Factorial
    The factorial of integer n, written as n! is defined as:
    n! = n * (n - 1) * (n - 2) * .... * 3 * 2 * 1
    
    Calculate and print the factorial of a given integer.

--- index.py
+++ index.py
@@ -1,12 +1,29 @@
 """
 PROBLEM STATEMENT 
+Extra Long Factorial
+
+The factorial of integer n, written as n! is defined as:
+n! = n * (n - 1) * (n - 2) * .... * 3 * 2 * 1
+
+Calculate and print the factorial of a given integer.
 
 CONDITIONS
 
 SAMPLE INPUT 
+Consists of a single integer n
 
 SAMPLE OUTPUT 
 
 CONSTRAINT
 """
 
+def extraLongFactorials(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+
+    return fact
+
+
+n = 30
+print(extraLongFactorials(n))
