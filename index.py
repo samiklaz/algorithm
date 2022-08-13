3dae3a28e1709cc2c1f1cadbf572f443106a0cb0
samiklaz <samuelasuquo80@gmail.com>
2022-08-13 17:02:46 +0100

    Square
    Watson likes to challenge Sherlock's math ability. He will provide a
    starting and ending value describing a range of integers. Sherlock
    must determine the number of square integers within that range, inclusive
    of the endpoints
    
    For example, the range is a = 24 and b = 49, inclusive. There are three
    square integers in the range 25, 36 and 49.

--- index.py
+++ index.py
@@ -1,5 +1,14 @@
 """
 PROBLEM STATEMENT 
+Square
+
+Watson likes to challenge Sherlock's math ability. He will provide a 
+starting and ending value describing a range of integers. Sherlock
+must determine the number of square integers within that range, inclusive
+of the endpoints
+
+For example, the range is a = 24 and b = 49, inclusive. There are three 
+square integers in the range 25, 36 and 49.
 
 CONDITIONS
 
@@ -10,3 +19,12 @@ SAMPLE OUTPUT
 CONSTRAINT
 """
 
+from math import *
+
+def squares(a, b):
+    return floor(sqrt(b)) - ceil(sqrt(a)) + 1
+
+
+a = 24
+b = 49
+print(squares(a, b))
