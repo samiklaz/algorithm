a279fdb61f559bf542f70ee762d46bbf9d10fe6e
samiklaz <samuelasuquo80@gmail.com>
2022-08-22 10:02:47 +0100

    Encryption
    An English text needs to be encrypted using the following Encryption scheme.
    
    First the spaces are removed from the text, let L be the length of the text.
    
    Then Characters are written into a grid, whose rows and columns has the
    following constraint.

--- index.py
+++ index.py
@@ -1,5 +1,13 @@
 """
 PROBLEM STATEMENT 
+Encryption
+
+An English text needs to be encrypted using the following Encryption scheme.
+
+First the spaces are removed from the text, let L be the length of the text.
+
+Then Characters are written into a grid, whose rows and columns has the
+following constraint.
 
 CONDITIONS
 
@@ -10,3 +18,25 @@ SAMPLE OUTPUT
 CONSTRAINT
 """
 
+from math import *
+
+def encryption(s):
+    n = len(s)
+    r = floor(sqrt(n))
+    c = ceil(sqrt(n))
+    result = []
+
+    for i in range(c):
+        temp = []
+        j = 0
+        while i + j < n:
+            temp.append(s[i+j])
+            j += c
+        print("temp ===  ", temp)
+        result.append("".join(temp))
+
+    return "".join(result)
+
+
+s = "If man was meant to stay on the ground god would"
+print(encryption(s))
