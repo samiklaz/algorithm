e74ab02abbfa1444041c50b6a72cdd561fc4183c
samiklaz <samuelasuquo80@gmail.com>
2022-08-29 11:44:24 +0100

    Chocolate Feast
    Little Bobby loves chocolate. He frequently goes to the favorite 5 & 10 store,
    Penny Aunties to buy them. They are having a promotion at Penny Aunties. If
    Bobby saves enough wrappers, he can turn them in for a free chocolate.

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
 """
 PROBLEM STATEMENT 
+Chocolate Feast
+
+Little Bobby loves chocolate. He frequently goes to the favorite 5 & 10 store,
+Penny Aunties to buy them. They are having a promotion at Penny Aunties. If
+Bobby saves enough wrappers, he can turn them in for a free chocolate.
 
 CONDITIONS
 
@@ -10,3 +15,26 @@ SAMPLE OUTPUT
 CONSTRAINT
 """
 
+def chocolateFeast(n, c, m):
+    """
+    Parameters:
+        n - money to be spent on the bar
+        c = cost of each bar
+        m = wrappers that can be turned in to receive a bar.
+    Output:
+        no of bars Bobby have eaten
+    """
+    choco = n // c
+    wraps = choco
+
+    while wraps >= m:
+        choco += wraps // m
+        wraps = wraps // m + wraps % m
+    
+    return choco
+
+
+n = 15
+c = 3
+m = 2
+print(chocolateFeast(n, c, m))
