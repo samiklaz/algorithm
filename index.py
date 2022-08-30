3f74fdaac9e2833182b5cf3a363016c63137dea7
samiklaz <samuelasuquo80@gmail.com>
2022-08-30 11:33:38 +0100

    Service Lane
    Calvin is driving his favorite vehicle on the 101 freeway. He notices that the
    check engine light on the vehicle is on, and he wants to service it immediately
    to avoid any risks. Luckily, a service lane runs parallel to the highway. The
    service lane varies in width along its length.
    
    You will be given an array of widths at points along the road (indices), then a
    list of indices of entry and exit points. Considering each entry and exit pair,
    calculate the minimum size vehicle that can travel that segment of the service
    lane safely.

--- index.py
+++ index.py
@@ -1,5 +1,16 @@
 """
 PROBLEM STATEMENT 
+Service Lane
+
+Calvin is driving his favorite vehicle on the 101 freeway. He notices that the
+check engine light on the vehicle is on, and he wants to service it immediately
+to avoid any risks. Luckily, a service lane runs parallel to the highway. The
+service lane varies in width along its length.
+
+You will be given an array of widths at points along the road (indices), then a 
+list of indices of entry and exit points. Considering each entry and exit pair,
+calculate the minimum size vehicle that can travel that segment of the service
+lane safely.
 
 CONDITIONS
 
@@ -10,3 +21,10 @@ SAMPLE OUTPUT
 CONSTRAINT
 """
 
+
+def serviceLane(n, cases):
+    result = []
+    for i, j in cases:
+        result.append(min(width[i : j+1]))
+
+    return result
